Ademola-Game-dev/godot-nvidia

NVIDIA DLSS & MIT Pathtracer

servers/rendering/renderer_scene_render.cpp
@@ -707,6 +707,24 @@ RSE::EnvironmentSDFGIYScale RendererSceneRender::environment_get_sdfgi_y_scale(R
 
 // Adjustments
 
+// Raytracing
+
+void RendererSceneRender::environment_set_raytracing(RID p_env, bool p_enable) {
+	environment_storage.environment_set_raytracing(p_env, p_enable);
+}
+
+bool RendererSceneRender::environment_get_raytracing_enabled(RID p_env) const {
+	return environment_storage.environment_get_raytracing_enabled(p_env);
+}
+
+void RendererSceneRender::environment_set_raytracing_params(RID p_env, const PackedFloat32Array &p_params) {
+	environment_storage.environment_set_raytracing_params(p_env, p_params);
+}
+
+PackedFloat32Array RendererSceneRender::environment_get_raytracing_params(RID p_env) const {
+	return environment_storage.environment_get_raytracing_params(p_env);
+}
+
 void RendererSceneRender::environment_set_adjustment(RID p_env, bool p_enable, float p_brightness, float p_contrast, float p_saturation, bool p_use_1d_color_correction, RID p_color_correction) {
 	environment_storage.environment_set_adjustment(p_env, p_enable, p_brightness, p_contrast, p_saturation, p_use_1d_color_correction, p_color_correction);
 }

maidopi-usagi/godot

servers/rendering/renderer_scene_render.cpp
@@ -707,6 +707,24 @@ RSE::EnvironmentSDFGIYScale RendererSceneRender::environment_get_sdfgi_y_scale(R
 
 // Adjustments
 
+// Raytracing
+
+void RendererSceneRender::environment_set_pathtracing(RID p_env, bool p_enable) {
+	environment_storage.environment_set_pathtracing(p_env, p_enable);
+}
+
+bool RendererSceneRender::environment_get_pathtracing_enabled(RID p_env) const {
+	return environment_storage.environment_get_pathtracing_enabled(p_env);
+}
+
+void RendererSceneRender::environment_set_pathtracing_params(RID p_env, const PackedFloat32Array &p_params) {
+	environment_storage.environment_set_pathtracing_params(p_env, p_params);
+}
+
+PackedFloat32Array RendererSceneRender::environment_get_pathtracing_params(RID p_env) const {
+	return environment_storage.environment_get_pathtracing_params(p_env);
+}
+
 void RendererSceneRender::environment_set_adjustment(RID p_env, bool p_enable, float p_brightness, float p_contrast, float p_saturation, bool p_use_1d_color_correction, RID p_color_correction) {
 	environment_storage.environment_set_adjustment(p_env, p_enable, p_brightness, p_contrast, p_saturation, p_use_1d_color_correction, p_color_correction);
 }